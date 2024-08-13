d2dd7ca568cb29e9237a11bab04f90811d63bc51
GwakByeongGuk <teereal@naver.com>
2024-08-13 16:37:42 +0900

    sqlalchemy - sungjuk 조회

--- sqlalchemy01.py
+++ sqlalchemy01.py
@@ -1,17 +1,13 @@
-from fastapi import FastAPI
+from typing import List
+from fastapi import FastAPI, Depends
+from pydantic import BaseModel
 from sqlalchemy import create_engine, Column, String, Integer
-from sqlalchemy.orm import sessionmaker, declarative_base
-
-# sqlalchemy
-# 파이썬용 ORM 라이브러리
-# sqlalchemy.org
+from sqlalchemy.orm import sessionmaker, declarative_base, Session
 
 # 데이터베이스 설정
 sqlite_url = 'sqlite:///python.db'
-engine = create_engine(sqlite_url,
-       connect_args={'check_same_thread': False}, echo=True)
-SessionLocal = sessionmaker(autocommit=False,
-            autoflush=False, bind=engine)
+engine = create_engine(sqlite_url, connect_args={'check_same_thread': False}, echo=True)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 # 데이터베이스 모델
 Base = declarative_base()
@@ -28,6 +24,26 @@ class Sungjuk(Base):
 # 데이터베이스 테이블 생성
 Base.metadata.create_all(bind=engine)
 
+# 데이터베이스 세션을 의존성으로 주입하기 위한 함수
+def get_db():
+    db = SessionLocal() # 데이터베이스 세션 객체 생성
+    try:
+        yield db # yield : 파이썬 제너레이터 객체
+                 # 함수가 호출될 때 비로소 객체를 반환(넘김)
+    finally:
+        db.close() # 데이터베이스 세션 닫음 (db 연결해제, 리소스 반환)
+
+# Pydantic 모델
+class SungjukModel(BaseModel):
+    sjng: int
+    name: str
+    kor: int
+    eng: int
+    mat: int
+
+    class Config:
+        orm_mode = True
+
 # FastAPI 메인
 app = FastAPI()
 
@@ -35,6 +51,13 @@ app = FastAPI()
 def index():
     return 'Hello, sqlalchemy!!'
 
+# 성적 조회
+# Depends : 의존성 주입 - db 세션 제공
+# => 코드 재사용성 향상, 관리 용이성 향상
+@app.get('/sj', response_model=List[SungjukModel])
+def read_sj(db: Session = Depends(get_db)):
+    sungjuks = db.query(Sungjuk).all()
+    return sungjuks
 
 if __name__ == "__main__":
     import uvicorn
